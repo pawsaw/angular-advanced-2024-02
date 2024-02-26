ae12ab8e29c10f82e1a9af9b7875fea0cbbe1f2b
Pawel Sawicki <public@psawicki.de>
2024-02-26 10:45:28 +0100

    book-details use store

--- src/app/book/store/book-collection.selectors.ts
+++ src/app/book/store/book-collection.selectors.ts
@@ -1,4 +1,7 @@
 import { createSelector } from '@ngrx/store';
 import { selectBookFeature } from './book.feature';
+import { Book } from '../models';
 
 export const selectBookCollection = createSelector(selectBookFeature, feature => feature.bookCollection.entities);
+export const selectBookByIsbn = (isbn: Book['isbn']) =>
+  createSelector(selectBookCollection, books => books.find(book => book.isbn === isbn));
